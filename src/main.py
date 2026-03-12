7f9e7fbdaba54343c4d35f3c819fc049029f4bb3
Pavlo Starobub <starodub.pavlo@alsodev.md>
2026-03-12 21:19:49 +0200

    refactor main

--- src/main.py
+++ src/main.py
@@ -4,12 +4,15 @@ import uvicorn
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
+from src.category import categories_router
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 app = FastAPI()
 app.mount("/src/media", StaticFiles(directory=BASE_DIR / "src" / "media"), name="media")
 app.mount("/src/static", StaticFiles(directory=BASE_DIR / "src" / "static"), name="static")
+app.include_router(categories_router)
 
 
 @app.get("/")
