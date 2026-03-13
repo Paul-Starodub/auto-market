e71f30dbff3840f01abee496ad8adb0946f30975
Pavlo Starobub <starodub.pavlo@alsodev.md>
2026-03-13 14:21:27 +0200

    refactor models

--- src/category/categories.py
+++ src/category/categories.py
@@ -8,7 +8,7 @@ from src.category.schemas import PaginatedCategoryResponse, Category
 from src.config import settings
 from src.models.dependencies import get_db
 
-router = APIRouter(prefix="/categories", tags=["Categories"])
+router = APIRouter(prefix="/categories", tags=["categories"])
 
 
 @router.get("/", response_model=PaginatedCategoryResponse)
@@ -34,4 +34,6 @@ async def get_category(db: Annotated[AsyncSession, Depends(get_db)], category_id
     category = await crud.get_category(db=db, category_id=category_id)
     if category is not None:
         return category
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Category not found")
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND, detail="Category not found"
+    )
