45b997f9e70cc516f53d5b5041b29608ed78e22b
Pavlo Starobub <starodub.pavlo@alsodev.md>
2026-03-13 21:44:50 +0200

    update crud category

--- src/category/crud.py
+++ src/category/crud.py
@@ -33,3 +33,26 @@ async def create_category(db: AsyncSession, category_create: schemas.CategoryCre
     await db.commit()
     await db.refresh(category)
     return category
+
+
+async def update_category(db: AsyncSession, category_id: int, category_update: schemas.CategoryCreate) -> Category:
+    stmt = select(Category).where(Category.id == category_id)
+    result = await db.execute(stmt)
+    category = result.scalar_one_or_none()
+    if not category:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Category not found")
+    if category.name != category_update.name:
+        stmt = select(Category).where(Category.name == category_update.name)
+        result = await db.execute(stmt)
+        existing_category = result.scalar_one_or_none()
+        if existing_category:
+            raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Category with this name already exists")
+    category.name = category_update.name
+    db.add(category)
+    try:
+        await db.commit()
+    except IntegrityError:
+        await db.rollback()
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Category with this name already exists")
+    await db.refresh(category)
+    return category
