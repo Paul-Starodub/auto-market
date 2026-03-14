fbef5a7606f74c50d6045506c78b3129c15ecb2e
Pavlo Starobub <starodub.pavlo@alsodev.md>
2026-03-14 16:41:37 +0200

    add crud functions for customer authentication and creation

---
+++ src/customer/crud.py
@@ -0,0 +1,26 @@
+from sqlalchemy import select, func
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src import models
+from src.customer.schemas import CustomerCreate
+from src.customer.secutity import hash_password
+
+
+async def get_customer_by_username(db: AsyncSession, username: str):
+    result = await db.execute(select(models.Customer).where(func.lower(models.Customer.username) == username.lower()))
+    return result.scalars().first()
+
+
+async def get_customer_by_email(db: AsyncSession, email: str):
+    result = await db.execute(select(models.Customer).where(models.Customer.email == email.lower()))
+    return result.scalars().first()
+
+
+async def create_customer(db: AsyncSession, customer: CustomerCreate):
+    new_customer = models.Customer(
+        username=customer.username, email=customer.email.lower(), password_hash=hash_password(customer.password)
+    )
+    db.add(new_customer)
+    await db.commit()
+    await db.refresh(new_customer)
+    return new_customer
