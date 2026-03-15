2c3912f79676eb39f13cfa29e62d2b51f7e44224
Pavlo Starobub <starodub.pavlo@alsodev.md>
2026-03-15 08:28:48 +0200

    add refresh token model and migration

--- src/models/customer.py
+++ src/models/customer.py
@@ -1,9 +1,11 @@
+from datetime import datetime, timezone, timedelta
 from typing import TYPE_CHECKING
 
-from sqlalchemy import String
+from sqlalchemy import String, DateTime, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship, validates
 
 from src.customer import validators
+from src.customer.utils import generate_secure_token
 from src.models.base import Base
 from src.models.mixins import CustomerRelationMixin
 
@@ -50,3 +52,37 @@ class Profile(CustomerRelationMixin, Base):
 
     def __repr__(self) -> str:
         return f"Profile(id={self.id}, first_name={self.first_name}, last_name={self.last_name})"
+
+
+class RefreshTokenModel(Base):
+    __tablename__ = "refresh_tokens"
+
+    token: Mapped[str] = mapped_column(
+        String(64), unique=True, default=generate_secure_token
+    )
+    expires_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        default=lambda: datetime.now(timezone.utc) + timedelta(days=1),
+    )
+    customer_id: Mapped[int] = mapped_column(
+        ForeignKey("customers.id", ondelete="CASCADE")
+    )
+
+    customer: Mapped[Customer] = relationship(back_populates="refresh_tokens")
+
+    @classmethod
+    def create(
+        cls, customer_id: int | Mapped[int], days_valid: int, token: str
+    ) -> "RefreshTokenModel":
+        """
+        Factory method to create a new RefreshTokenModel instance.
+
+        This method simplifies the creation of a new refresh token by calculating
+        the expiration date based on the provided number of valid days and setting
+        the required attributes.
+        """
+        expires_at = datetime.now(timezone.utc) + timedelta(days=days_valid)
+        return cls(customer_id=customer_id, expires_at=expires_at, token=token)
+
+    def __repr__(self) -> str:
+        return f"<RefreshTokenModel(id={self.id}, token={self.token}, expires_at={self.expires_at})>"
