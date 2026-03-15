4c2600e8339c59bd7bda55ff26db8e63a4747c2d
Pavlo Starobub <starodub.pavlo@alsodev.md>
2026-03-15 12:54:56 +0200

    refactor crud

--- src/customer/crud.py
+++ src/customer/crud.py
@@ -3,22 +3,30 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from src import models
 from src.customer.schemas import CustomerCreate
-from src.customer.secutity import hash_password
+from src.customer.security.secutity import hash_password
 
 
 async def get_customer_by_username(db: AsyncSession, username: str):
-    result = await db.execute(select(models.Customer).where(func.lower(models.Customer.username) == username.lower()))
+    result = await db.execute(
+        select(models.Customer).where(
+            func.lower(models.Customer.username) == username.lower()
+        )
+    )
     return result.scalars().first()
 
 
 async def get_customer_by_email(db: AsyncSession, email: str):
-    result = await db.execute(select(models.Customer).where(models.Customer.email == email.lower()))
+    result = await db.execute(
+        select(models.Customer).where(models.Customer.email == email.lower())
+    )
     return result.scalars().first()
 
 
 async def create_customer(db: AsyncSession, customer: CustomerCreate):
     new_customer = models.Customer(
-        username=customer.username, email=customer.email.lower(), password_hash=hash_password(customer.password)
+        username=customer.username,
+        email=customer.email.lower(),
+        password_hash=hash_password(customer.password),
     )
     db.add(new_customer)
     await db.commit()
