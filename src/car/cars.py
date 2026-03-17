86e72990e84d3ae75829f701bc1b1b0648c8ee23
Pavlo Starobub <starodub.pavlo@alsodev.md>
2026-03-17 14:46:33 +0200

    refactor schemas

--- src/car/cars.py
+++ src/car/cars.py
@@ -4,7 +4,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.car import crud
-from src.car.schemas import CarCreate
+from src.car.schemas import CarCreate, CarImageCreate
 from src.models.dependencies import get_db
 
 router = APIRouter(prefix="/cars", tags=["cars"])
@@ -15,3 +15,12 @@ async def create_car(
     car_create: CarCreate, db: Annotated[AsyncSession, Depends(get_db)]
 ):
     return await crud.create_car(db=db, car_create=car_create)
+
+
+@router.post("{car_id}/images/")  # TODO
+async def create_car_image(
+    car_id: int,
+    car_image_create: CarImageCreate,
+    db: Annotated[AsyncSession, Depends(get_db)],
+):
+    return await crud.create_car_image(db=db, car_image_create=car_image_create)
--- src/car/crud.py
+++ src/car/crud.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src import models
-from src.car.schemas import CarCreate
+from src.car.schemas import CarCreate, CarImageCreate
 
 
 async def create_car(db: AsyncSession, car_create: CarCreate):
@@ -10,3 +10,14 @@ async def create_car(db: AsyncSession, car_create: CarCreate):
     await db.commit()
     await db.refresh(new_car)
     return new_car
+
+
+async def create_car_image(
+    db: AsyncSession,
+    car_image_create: CarImageCreate,
+):  # TODO
+    new_car_image = models.CarImage(**car_image_create.model_dump())
+    db.add(new_car_image)
+    await db.commit()
+    await db.refresh(new_car_image)
+    return new_car_image
