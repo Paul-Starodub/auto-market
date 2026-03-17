b02532465468de95fbe165202c676f73c53b10e6
Pavlo Starobub <starodub.pavlo@alsodev.md>
2026-03-17 15:16:02 +0200

    refactor crud

--- src/car/crud.py
+++ src/car/crud.py
@@ -1,7 +1,25 @@
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src import models
-from src.car.schemas import CarCreate, CarImageCreate
+from src.car.schemas import CarCreate
+
+
+async def get_car_by_id(db: AsyncSession, car_id: int):
+    result = await db.execute(select(models.Car).where(models.Car.id == car_id))
+    return result.scalars().first()
+
+
+# async def get_car_by_id(db: AsyncSession, car_id: int):  # TODO for future
+#     result = await db.execute(
+#         select(models.Car)
+#         .options(
+#             selectinload(models.Car.images),
+#             selectinload(models.Car.category),
+#         )
+#         .where(models.Car.id == car_id)
+#     )
+#     return result.scalars().first()
 
 
 async def create_car(db: AsyncSession, car_create: CarCreate):
@@ -12,12 +30,8 @@ async def create_car(db: AsyncSession, car_create: CarCreate):
     return new_car
 
 
-async def create_car_image(
-    db: AsyncSession,
-    car_image_create: CarImageCreate,
-):  # TODO
-    new_car_image = models.CarImage(**car_image_create.model_dump())
-    db.add(new_car_image)
+async def add_car_images(db: AsyncSession, car_id: int, file_paths: list[str]):
+    images = [models.CarImage(file_path=path, car_id=car_id) for path in file_paths]
+    db.add_all(images)
     await db.commit()
-    await db.refresh(new_car_image)
-    return new_car_image
+    return images
