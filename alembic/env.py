82ad3fbf7d23384df4ed1b02efad19bfc9888210
Pavlo Starobub <starodub.pavlo@alsodev.md>
2026-03-19 18:55:44 +0200

    refactor alembic

--- alembic/env.py
+++ alembic/env.py
@@ -6,14 +6,14 @@ from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
-from src.config import settings
+from src.core.config import settings
 from src.database import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
 
-config.set_main_option("sqlalchemy.url", settings.DATABASE_URL)
+config.set_main_option("sqlalchemy.url", settings.db.DATABASE_URL)
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
